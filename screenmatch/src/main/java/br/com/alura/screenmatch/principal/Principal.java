9794a372ea32c7299209a16a63de5260908823d8
Danylo Baziewicz <danxbazi@gmail.com>
2025-05-26 22:21:51 -0300

    refactor: transforma DadosEpisodio em objetos Episodio com mapeamento da temporada

--- screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import br.com.alura.screenmatch.util.ApiKeyLoader;
@@ -10,8 +11,10 @@ import br.com.alura.screenmatch.util.ApiKeyLoader;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -26,7 +29,7 @@ public class Principal {
     public void exibeMenu() {
         System.out.println("Digite o nome da série que deseja pesquisar: ");
         String nomeSerie = URLEncoder.encode(scan.nextLine(), StandardCharsets.UTF_8);
-        String urlSerie = BASE_URL+nomeSerie+API_KEY;
+        String urlSerie = BASE_URL+nomeSerie+"&apikey="+API_KEY;
         var json = consumoApi.obterDados(urlSerie);
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
@@ -34,7 +37,7 @@ public class Principal {
         List<DadosTemporada> temporadas = new ArrayList<>();
 
 		for (int i = 1; i<=dados.totalTemporadas(); i++) {
-			urlSerie = BASE_URL+nomeSerie+SEASON+i+API_KEY;
+			urlSerie = BASE_URL+nomeSerie+SEASON+i+"&apikey="+API_KEY;
 			json = consumoApi.obterDados(urlSerie);
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
@@ -47,6 +50,25 @@ public class Principal {
 //                System.out.println(episodiosTemporada.get(j).titulo());
 //            }
 //        }
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//
+//        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+//                .flatMap(t -> t.episodios().stream())
+//                .collect(Collectors.toList());
+//
+//        System.out.println("\nTop 5 episódios da série: "+dados.titulo());
+//        dadosEpisodios.stream()
+//                .filter(e -> !e.avaliacao().equals("N/A"))
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                .limit(5)
+//                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
     }
 }
