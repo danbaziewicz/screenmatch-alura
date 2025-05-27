a52df5cc61867bb6c1eecbb9fb98d9224eae455b
Danylo Baziewicz <danxbazi@gmail.com>
2025-05-26 22:22:35 -0300

    refactor: remove constantes de URL e API key da classe

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,7 +1,6 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.principal.Principal;
-import br.com.alura.screenmatch.util.ApiKeyLoader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +11,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
-
-	private static final String API_KEY = ApiKeyLoader.loadApiKey();
-	private static final String BASE_URL = "https://www.omdbapi.com/?t=";
 
 	@Override
 	public void run(String... args) throws Exception {
